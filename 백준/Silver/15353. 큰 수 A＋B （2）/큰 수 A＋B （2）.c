9d696985e7d22723c84df8357b7ff68d8b75358f
Hyeon-hak Kim <96914905+ooMia@users.noreply.github.com>
2022-12-21 14:11:58 +0900

    [Silver III] Title: 큰 수 A+B (2), Time: 0 ms, Memory: 1136 KB -BaekjoonHub

--- 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.c
+++ 백준/Silver/15353. 큰 수 A＋B （2）/큰 수 A＋B （2）.c
@@ -1,34 +1,39 @@
-#define MAX(a,b) (a>b?a:b)
 #include <stdio.h>
 #include <string.h>
-char str1[10000 + 1] = { 0, };
-char str2[10000 + 1] = { 0, };
-char str3[10000 + 2] = { 0, };
+#include <malloc.h>
+
+char* add_integerString(const char* num1, const char* num2)
+{
+	const int len_num1 = (int)strlen(num1);
+	const int len_num2 = (int)strlen(num2);
+	const int len_num3 = (len_num1 > len_num2 ? len_num1 : len_num2) + 1;
+
+	char* num3 = (char*)malloc((size_t)len_num3 + 1);
+	if (num3 != NULL) memset(num3, 0, (size_t)len_num3 + 1);
+	else return NULL;
+
+	// num1, num2, num3 = {0, }
+
+	char* ptr_num3_1 = num3 + len_num3 - 1;
+	for (int i1 = len_num1 - 1; i1 >= 0; --i1) *ptr_num3_1-- += num1[i1] - '0';
+
+	char* ptr_num3_2 = num3 + len_num3 - 1;
+	for (int i2 = len_num2 - 1; i2 >= 0; --i2) *ptr_num3_2-- += num2[i2] - '0';
+
+	for (int i3 = len_num3 - 1; i3 >= 0; --i3) {
+		if (num3[i3] >= 10) num3[i3] -= 10, num3[i3 - 1] += 1;
+		num3[i3] += '0';
+	}
+
+	while (*num3 == '0') ++num3;
+	return num3;
+}
+
+char g_str1[10000 + 1] = { 0, };
+char g_str2[10000 + 1] = { 0, };
+
 int main() {
-	scanf("%s %s", str1, str2);
-	int index_1 = strlen(str1) - 1;
-	int index_2 = strlen(str2) - 1;
-	int index_3 = MAX(index_1, index_2) + 1, index_4 = index_3;
-	
-	while (index_1 >= 0 && index_2 >= 0)
-	{	
-		int n1 = str1[index_1--] - '0';
-		int n2 = str2[index_2--] - '0';
-
-		str3[index_3--] += n1 + n2;
-		if (str3[index_3 + 1] >= 10) { str3[index_3 + 1] -= 10; str3[index_3] += 1; }
-	}
-
-	while (index_1 >= 0) {
-		str3[index_3--] += str1[index_1--] - '0';
-		if (str3[index_3 + 1] >= 10) { str3[index_3 + 1] -= 10; str3[index_3] += 1; }
-	}
-	while (index_2 >= 0) {
-		str3[index_3--] += str2[index_2--] - '0';
-		if (str3[index_3 + 1] >= 10) { str3[index_3 + 1] -= 10; str3[index_3] += 1; }
-	}
-	for (int i = 0; i <= index_4; ++i) str3[i] += '0';
-
-	printf("%s", str3[0] == '0' ? str3 + 1 : str3);
+	scanf("%s %s", g_str1, g_str2);
+	printf("%s", add_integerString(g_str1, g_str2));
 	return 0;
 }
