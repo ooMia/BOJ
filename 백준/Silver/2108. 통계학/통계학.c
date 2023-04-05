bc7a7122c9e6b4666efc53d1d5fc02d1ea34e00e
Hyeon-hak Kim <07ily@naver.com>
2023-04-05 13:41:58 +0900

    [Silver III] Title: 통계학, Time: 128 ms, Memory: 5028 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.c
+++ 백준/Silver/2108. 통계학/통계학.c
@@ -1,54 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-#define count(N) (CNT[4000+(N)])
-
-int sum, cumul_count, median_count;
-short min, max, median;
-short mode[3] = { 0, };
-int CNT[8001] = { 0, };
-
-void calcMode(short num) {
-	short M; int cM;
-	int cNum = ++count(num);
-
-	for (char i = 0; i <= 1; i++) {
-		M = mode[i];
-		if (num == M) break; else cM = count(M);
-		if (cNum > cM || (cNum == cM && num < M)) {
-			mode[i] = num, mode[i + 1] = M; break;
-		}
-	}
-}
-
-short calcAvg(int N, int sum) {
-	int L = sum / N;
-	double R = (double)sum / N - L;
-	char sign = (L < 0) ? -1 : +1;
-
-	return (short)(L + sign * (0.5 <= sign * R));
-}
-
-int main(void)
+int compare(const void* first, const void* second)
 {
-	int N; short in[2]; scanf(" %d %hd", &N, in);
-	if (N == 1) { printf("%hd\n%hd\n%hd\n0", *in, *in, *in); return 0; }
+	int* a = (int*)first;
+	int* b = (int*)second;
 
-	sum = min = max = mode[0] = *in, ++count(*in);
+	if (*a < *b)
+		return -1;
+	else if (*a > *b)
+		return 1;
+	else
+		return 0;
+}
 
-	for (int iter = 1; iter < N; iter += 2) {
-		scanf(" %hd %hd", in, in + 1);
+int arith(int list[], int n)
+{
+	double sum = 0;
 
-		for (char i = 0; i < 2; ++i) {
-			short s = in[i];
+	for (int i = 0; i < n; i++)
+	{
+		sum += (list[i]);
+	}
 
-			sum += s;
-			if (s < min) min = s; else if (s > max) max = s;
-			calcMode(s);
+	return round(sum / n);
+}
+
+int median(int list[], int n)
+{
+	if (n == 1)
+		return list[0];
+	else
+		return list[(n + 1) / 2 - 1];
+}
+
+int Mode(int list[], int n)
+{
+	int ar[8001] = { 0 };
+	int i, idx, max = 0, cnt = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		idx = list[i] + 4000;
+		ar[idx] += 1;
+		
+		if (ar[idx] > max)
+			max = ar[idx];
+	}
+
+	for (i = 0, idx = 0; i < 8001 ; i++)
+	{
+		if (ar[i] == 0)
+			continue;
+
+		if (ar[i] == max)
+		{
+			if (cnt == 0)
+			{
+				idx = i;
+				cnt += 1;
+			}
+			else if (cnt == 1)
+			{
+				idx = i;
+				break;
+			}
 		}
 	}
-	median = min - 1, cumul_count = 0, median_count = (N + 1) / 2;
-	while (cumul_count < median_count) cumul_count += count(++median);
+	return idx - 4000;
 
-	printf("%hd\n%hd\n%hd\n%hd", calcAvg(N,sum), median, (count(mode[0]) == count(mode[1])) ? mode[1] : mode[0], max - min);
+}
+
+int range(int list[], int n)
+{
+	int max = list[n - 1];
+	int min = list[0];
+
+	return max - min;
+}
+
+int main()
+{
+	int i, n;
+	int* list;
+
+	scanf("%d", &n);
+	list = (int*)calloc(n, sizeof(int));
+
+	for (i = 0; i < n; i++)
+	{
+		scanf(" %d", &list[i]);
+	}
+	
+	qsort(list, n, sizeof(list[0]), compare);
+
+	printf("%d\n", arith(list, n));
+	printf("%d\n", median(list, n));
+	printf("%d\n", Mode(list, n));
+	printf("%d\n", range(list, n));
+	
 	return 0;
 }
