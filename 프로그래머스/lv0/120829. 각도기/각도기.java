ffea88563ccaf09661e247cc13104de8977a6273
Hyeon-hak Kim <07ily@naver.com>
2023-04-11 19:40:17 +0900

    [level 0] Title: 각도기, Time: 0.10 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,20 +1,26 @@
-import java.util.*;
+import java.util.Map;
 
-class Solution {
-    public int solution(int angle) {
-        
-        Map<String, Integer> dict = Map.of(
-            "undefined", -1,
-            "예각", 1,
-            "직각", 2,
-            "둔각", 3,
-            "평각", 4
-        );
+public class Solution {
 
-        if (angle  < 90 ) return dict.get("예각");
-        else if (angle == 90 ) return dict.get("직각");
-        else if (angle == 180) return dict.get("평각");
-        else if (angle  > 90 ) return dict.get("둔각");
-        else return dict.get("undefined");
-    }
+  public int solution(int angle) {
+    // 각도에 따른 결과를 저장하는 Map 생성
+    Map<String, Integer> dict = Map.of(
+        "undefined", -1,
+        "예각", 1,
+        "직각", 2,
+        "둔각", 3,
+        "평각", 4);
+
+    int res = dict.get("undefined");
+    if (angle < 90)
+      res = dict.get("예각");
+    else if (angle == 90)
+      res = dict.get("직각");
+    else if (angle == 180)
+      res = dict.get("평각");
+    else if (angle > 90)
+      res = dict.get("둔각");
+    return res;
+  }
+
 }
