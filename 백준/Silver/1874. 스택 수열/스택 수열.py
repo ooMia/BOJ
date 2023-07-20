4b7eb0c850180a173410a2b595b64b61fb66f167
Hyeon-hak Kim <07ily@naver.com>
2023-07-21 06:16:07 +0900

    [Silver II] Title: 스택 수열, Time: 160 ms, Memory: 43436 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,14 +2,15 @@ import sys
 
 arr = list(map(int, sys.stdin.readlines()))
 
-IN = [n for n in range(1, arr[0] + 1)]
+IN = 1
 stack = [0]
 res = []
 
 for ar in arr[1:]:
     while stack[-1] < ar:
         res.append('+')
-        stack.append(IN.pop(0))
+        stack.append(IN)
+        IN += 1
 
     if stack[-1] == ar:
         res.append('-')
@@ -17,7 +18,7 @@ for ar in arr[1:]:
     else:
         break
 
-if len(IN) == 0 and len(stack) == 1:
+if IN == arr[0] + 1 and len(stack) == 1:
     print(*res, sep='\n')
 else:
     print('NO')
