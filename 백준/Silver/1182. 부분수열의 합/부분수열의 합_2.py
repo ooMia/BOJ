0ddf5b658e4e8262d2fb4258fb34c07cc5b3824f
Hyeon-hak Kim <07ily@naver.com>
2023-07-28 12:17:50 +0900

    Create 부분수열의 합_2.py
    BackTracking practice

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합_2.py
@@ -0,0 +1,45 @@
+import sys
+
+
+def search(nList, partialSum, diffSum):
+
+    if len(nList) < 1:
+        return 1 if partialSum == target else 0
+
+    # 모든 탐색은 순차적으로, 즉 최솟값에서부터 최댓값 방향으로 진행한다.
+
+    # nList[0]에 대한 선택x/o로 나누어 재귀적 호출
+
+    # 탐색의 각 단계는 각 요소의 선택 여부에 따라
+    #   선택된 수열의 합 partialSum,
+    #   선택이 결정되지 않은 나머지 수열의 합 diffSum
+    # 에 대한 상태를 최신화한다.
+
+    # 각 요소 e는 그 크기에 따라 적용 방식이 달라진다.
+    # if e >= 0: partialSum += e, diffSum -= e
+    # if e < 0: partialSum -= e, diffSum += e
+    # > 일반화하면 partialSum += abs(e), diffSum += abs(e)
+
+    return search(nList[1:], partialSum, diffSum) +\
+        search(nList[1:], partialSum + abs(nList[0]), diffSum + abs(nList[0]))
+
+
+IN = list(map(lambda x: list(map(int, x.split())), sys.stdin.readlines()))
+
+# 주어진 목표값: target
+N, target = IN[0]
+
+# nList 오름차순 정렬
+nList = sorted(IN[1])
+
+# 순차 탐색하여 음수의 총합 negSum과 양수의 총합 posSum을 구한다.
+negSum, posSum = 0, 0
+for n in nList:
+    if n < 0: negSum += n
+    else: posSum += n
+
+# 초기 탐색은 partialSum = negSum, diffSum = posSum에서 시작한다.
+# 단, 음수들의 총합 negSum이 target과 같다면 결과값에서 1을 뺀다.
+
+res = search(nList, negSum, posSum) - (1 if target == 0 else 0)
+print(res)
