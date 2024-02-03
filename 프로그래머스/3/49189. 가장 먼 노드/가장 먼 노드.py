67ecd09e477ad62401b018e58d7b7336fec9aaf4
Hyeon-hak Kim <07ily@naver.com>
2024-02-04 07:40:48 +0900

    [level 3] Title: 가장 먼 노드, Time: 47.22 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,38 @@
+def solution(n, edges):
+    graph = init_graph(edges)
+    
+    visited_nodes = set()
+    current_nodes = set()
+    next_nodes = set([1])
+    
+    while not is_all_leaf(graph, next_nodes):
+        
+        current_nodes = next_nodes.copy()
+        next_nodes.clear()
+        
+        for current_node in current_nodes:
+            visited_nodes.add(current_node)
+            next_nodes |= graph[current_node]
+        next_nodes -= visited_nodes
+    
+    return len(current_nodes)
+
+
+def is_all_leaf(graph, nodes):
+    for node in nodes:
+        if node in graph.keys():
+            return False
+    return True
+
+
+def init_graph(edges):
+    graph = dict()
+    for edge in edges:
+        [node1, node2] = edge
+        if node1 not in graph.keys():
+            graph[node1] = set()
+        if node2 not in graph.keys():
+            graph[node2] = set()
+        graph[node1].add(node2)
+        graph[node2].add(node1)
+    return graph
