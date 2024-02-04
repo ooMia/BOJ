cd696d156f141b4c8c686d352eeb1edf6c3bdbce
Hyeon-hak Kim <07ily@naver.com>
2024-02-05 02:14:09 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -1,12 +1,20 @@
 def solution(n, l, r):
-    answer = r-l+1
-    for num in range(l-1,r):
-        while num>=1:
-            a,b=divmod(num,5)
-            if b==2 or a==2:
-                answer-=1
-                break
-            num=a
+    if n == 1:
+        return sum([1, 1, 0, 1, 1][l - 1: r])
 
+    block_size = 5 ** (n - 1)
+    block_n_start = (l - 1) // block_size + 1
+    block_n_end = (r - 1) // block_size + 1
+    res = 0
 
-    return answer
+    for block_n in range(block_n_start, block_n_end + 1):
+        if block_n != 3:
+            if block_n_start == block_n_end:
+                res += solution(n - 1, (l - 1) % block_size + 1, (r - 1) % block_size + 1)
+            elif block_n == block_n_start:
+                res += solution(n - 1, (l - 1) % block_size + 1, block_size)
+            elif block_n == block_n_end:
+                res += solution(n - 1, 1, (r - 1) % block_size + 1)
+            else:
+                res += 4 ** (n - 1)
+    return res
