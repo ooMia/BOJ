23d7825ee045aad5508b3abdad81cbc0129cf1fe
Hyeon-hak Kim <07ily@naver.com>
2024-02-04 09:13:57 +0900

    [level 2] Title: 오픈채팅방, Time: 105.77 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,35 +1,36 @@
 def solution(records):
-    users = dict()
     answer = list()
-
-    for record in records:
-        init_users(users, *record.split())
-    for record in records:
-        run(answer, users, *record.split())
+    [new_records, users] = init(records)
+    for record in new_records:
+        answer.append(run(users, *record.split()))
     return answer
 
-def init_users(users, command, user_id, user_name=None):
-    if command == "Enter" or command == "Change":
+
+def init(records):
+    [new_records, users] = [list(), dict()]
+    for record in records:
+        init_user(new_records, users, *record.split())        
+    return [new_records, users]
+
+
+def init_user(new_records, users, command, user_id, user_name=None):
+    if command == "Enter":
         users[user_id] = user_name
+        new_records.append(f"{command} {user_id}")
+    elif command == "Change":
+        users[user_id] = user_name
+    elif command == "Leave":
+        new_records.append(f"{command} {user_id}")
     pass
 
 
-def run(answer, users, command, user_id, user_name=None):
+def run(users, command, user_id):
+    welcome_message = lambda name: f"{name}님이 들어왔습니다."
+    goodbye_message = lambda name: f"{name}님이 나갔습니다."
+    
     if command == "Enter":
-        answer.append(run_enter(users, user_id))
+        return welcome_message(users[user_id])
     elif command == "Leave":
-        answer.append(run_leave(users, user_id)) 
-    elif command == "Change":
-        pass
+        return goodbye_message(users[user_id])
     else: 
         return "Undefined Command Error"
-
-    
-def run_enter(users, user_id):
-    welcome_message = lambda name: f"{name}님이 들어왔습니다."
-    return welcome_message(users[user_id])
-
-def run_leave(users, user_id):
-    goodbye_message = lambda name: f"{name}님이 나갔습니다."
-    return goodbye_message(users[user_id])
-
