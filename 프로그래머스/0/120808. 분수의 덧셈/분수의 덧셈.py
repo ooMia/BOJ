2d5774dfd89c0ace0d165fc397cd68d5e7f5fbcd
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-05-03 13:13:08 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,9 @@
 # 두 분수를 더한 값을 기약 분수로 나타냈을 때 분자와 분모
-def solution(numer1, denom1, numer2, denom2):
+def solution(numer1:int, denom1:int, numer2:int, denom2:int) -> list:
     denom3 = denom1 * denom2
     numer3 = numer1 * denom2 + numer2 * denom1
     gcd3 = gcd(denom3, numer3)
-    
-    denom3 //= gcd3
-    numer3 //= gcd3
-    return [numer3, denom3]
+    return [numer3 // gcd3, denom3 // gcd3]
 
 
 def gcd(a, b):
