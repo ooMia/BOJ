93b6dbca8709fcc35bc4c4424e34b44dcdd0e6eb
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-05-03 13:45:12 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,4 +1,6 @@
 # 문자열 my_string에서 모음을 제거한 문자열
 def solution(my_string: str) -> str:
-	vowel = ['a', 'e', 'i', 'o', 'u']
-	return ''.join([i for i in my_string if i not in vowel])
+	vowels = 'aeiou'
+	for i in vowels:
+		my_string = my_string.replace(i, '')
+	return my_string
