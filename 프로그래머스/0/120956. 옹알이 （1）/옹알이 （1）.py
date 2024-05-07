ccdb768ef54fd31446499742884aad9516511ea9
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-05-07 15:07:57 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -7,14 +7,41 @@ def solution(babbling: list) -> int:
     """
 
     word_set = {"aya", "ye", "woo", "ma"}
-    pool = set()
-    import itertools
-    for k in range(1, len(word_set) + 1):
-        for word in itertools.permutations(word_set, k):
-            pool.add("".join(word))
+    trie = make_trie(word_set)
 
     answer = 0
-    for babble in babbling:
-        if babble in pool:
-            answer += 1
+    for sentence in babbling:
+        answer += 1 if is_valid(trie, sentence) else 0
     return answer
+
+
+def make_trie(word_set: set) -> dict:
+    """
+    :param word_set: 단어 집합
+    :return: 검색 트리
+    """
+    trie = {}
+    for word in word_set:
+        node = trie
+        for i, ch in enumerate(word):
+            if i == len(word) - 1:
+                node[ch] = True
+                break
+            node = node.setdefault(ch, {})
+    return trie
+
+
+def is_valid(trie: dict, s: str) -> bool:
+    """
+    :param trie: 검색 트리
+    :param s: 알파벳 소문자 문자열
+    :return: s가 trie 내 문자열로만 이루어져 있는지 여부
+    """
+    node = trie
+    for ch in s:
+        if ch not in node:
+            return False
+        node = node[ch]
+        if node is True:
+            node = trie
+    return node is trie
