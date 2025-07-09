a3a808c06654540aa69d498a77d4f961ef1b5682
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-09 20:22:35 +0900

    [Silver IV] Title: 스택 2, Time: 1132 ms, Memory: 313308 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,145 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+
+interface IParser {
+    QInput next();
+
+    boolean hasNext();
+}
+
+interface IRunner {
+    void run();
+}
+
+public class Main {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final IParser parser = new Parser(br);
+    static final IRunner runner = new Runner(parser, bw);
+
+    public static void main(String[] args) throws IOException {
+        runner.run();
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+class Parser implements IParser {
+    final BufferedReader reader;
+    final QInput cache = new QInput(0, null);
+    boolean hasNextLine = true;
+
+    public Parser(BufferedReader br) {
+        this.reader = br;
+        try {
+            reader.readLine();
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public QInput next() {
+        try {
+            String line = reader.readLine();
+            if (line == null) {
+                hasNextLine = false;
+                return null;
+            }
+            String[] parts = line.split(" ");
+            int a = Integer.parseInt(parts[0]);
+            Integer b = parts.length > 1 ? Integer.parseInt(parts[1]) : null;
+            cache.set(a, b);
+            return cache;
+        } catch (IOException e) {
+            hasNextLine = false;
+            return null;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNextLine;
+    }
+}
+
+// record pattern
+class QInput {
+    private int opCode;
+    private Integer operand;
+
+    public QInput(int opCode, Integer operand) {
+        this.opCode = opCode;
+        this.operand = operand;
+    }
+
+    public int opCode() {
+        return opCode;
+    }
+
+    public Integer operand() {
+        return operand;
+    }
+
+    public void set(int opCode, Integer operand) {
+        this.opCode = opCode;
+        this.operand = operand;
+    }
+}
+
+class Runner implements IRunner {
+    final IParser parser;
+    final BufferedWriter writer;
+    final ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+    public Runner(IParser parser, BufferedWriter bw) {
+        this.parser = parser;
+        this.writer = bw;
+    }
+
+    void iterate() throws IOException {
+        QInput input = parser.next();
+        if (input == null)
+            return;
+
+        switch (input.opCode()) {
+            case 1:
+                stack.push(input.operand());
+                break;
+            case 2:
+                writer.write(String.valueOf(stack.isEmpty() ? -1 : stack.pop()));
+                writer.newLine();
+                break;
+            case 3:
+                writer.write(String.valueOf(stack.size()));
+                writer.newLine();
+                break;
+            case 4:
+                writer.write(String.valueOf(stack.isEmpty() ? 1 : 0));
+                writer.newLine();
+                break;
+            case 5:
+                writer.write(String.valueOf(stack.isEmpty() ? -1 : stack.peek()));
+                writer.newLine();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operation code: " + input.opCode());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (parser.hasNext()) {
+                iterate();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
