3efb96a8c3dce5cca3ffe430614c317c015e1c82
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-23 07:23:19 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1064 ms, Memory: 85728 KB -BaekjoonHub

---
+++ Java/백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,161 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int T = 1;
+            for (int i = 0; i < T; ++i) {
+                IRunner runner = new Runner(br, bw);
+                runner.run();
+                runner.flush();
+            }
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Runner implements IRunner {
+    final IReader reader;
+    final BufferedWriter bw;
+    final StringBuilder sb = new StringBuilder();
+
+    final int N;
+    final int[] dp;
+    final boolean[] visited;
+    final Map<Integer, List<Integer>> nodes;
+
+    Runner(BufferedReader br, BufferedWriter bw) {
+        this.reader = new Reader(br);
+        this.bw = bw;
+        try {
+            N = reader.readInts()[0];
+            dp = new int[N + 1];
+            visited = new boolean[N + 1];
+            nodes = new TreeMap<>();
+            for (int i = 0; i < N - 1; ++i) {
+                var line = reader.readIntegers();
+                var nodeA = line.get(0);
+                var nodeB = line.get(1);
+
+                if (!nodes.containsKey(nodeA)) {
+                    nodes.put(nodeA, new ArrayList<>());
+                }
+                nodes.get(nodeA).add(nodeB);
+
+                if (!nodes.containsKey(nodeB)) {
+                    nodes.put(nodeB, new ArrayList<>());
+                }
+                nodes.get(nodeB).add(nodeA);
+            }
+            sb.ensureCapacity(20);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void flush() {
+        try {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() throws IOException {
+        visit(1);
+
+        for (int i = 2; i <= N; ++i) {
+            sb.append(dp[i]).append('\n');
+        }
+    }
+
+    private void visit(int node) {
+        visited[node] = true;
+
+        for (int child : nodes.get(node)) {
+            if (visited[child]) {
+                continue;
+            }
+            dp[child] = node;
+            visit(child);
+        }
+    }
+}
+
+class Reader implements IReader {
+    private BufferedReader br;
+
+    public Reader(BufferedReader br) {
+        this.br = br;
+    }
+
+    @Override
+    public void skipLine() throws IOException {
+        br.readLine();
+    }
+
+    @Override
+    public Stream<String> lines() throws IOException {
+        return br.lines();
+    }
+
+    @Override
+    public String line() throws IOException {
+        return br.readLine();
+    }
+
+    @Override
+    public int[] readInts() throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        int[] ints = new int[tokens.length];
+        for (int i = 0; i < tokens.length; ++i) {
+            ints[i] = Integer.parseInt(tokens[i]);
+        }
+        return ints;
+    }
+
+    @Override
+    public List<Integer> readIntegers() throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        var res = new ArrayList<Integer>(tokens.length);
+        for (String token : tokens) {
+            res.add(Integer.valueOf(token));
+        }
+        return res;
+    }
+}
+
+interface IRunner {
+    void run() throws IOException;
+
+    void flush();
+}
+
+interface IReader {
+    void skipLine() throws IOException;
+
+    Stream<String> lines() throws IOException;
+
+    String line() throws IOException;
+
+    List<Integer> readIntegers() throws IOException;
+
+    int[] readInts() throws IOException;
+}
