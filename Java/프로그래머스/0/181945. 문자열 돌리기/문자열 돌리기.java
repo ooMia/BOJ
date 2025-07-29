b265d7d730dc39082a9628fe7127ecaf95099518
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-07-29 14:12:00 +0900

    [level 0] Title: 문자열 돌리기, Time: 207.28 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] cs = sc.next().toCharArray();
+       
+        for (char c : cs){
+            System.out.println(c);
+        }
+    }
+}
