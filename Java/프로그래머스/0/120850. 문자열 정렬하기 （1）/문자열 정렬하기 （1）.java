f196f4598fb71364e5f13f51ebe7053c04aaff3b
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-06 02:11:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.83 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        List<Integer> numbers = new ArrayList<>();
+        for (char c : my_string.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numbers.add(Character.getNumericValue(c));
+            }
+        }
+        Collections.sort(numbers);
+        return numbers.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
