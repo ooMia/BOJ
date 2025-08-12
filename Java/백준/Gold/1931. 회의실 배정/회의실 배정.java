5e720d8f1a3be6f083e2dc673f3913146357a0ba
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-12 14:37:30 +0900

    [Gold V] Title: 회의실 배정, Time: 644 ms, Memory: 52352 KB -BaekjoonHub

---
+++ Java/백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,158 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int T = 1;
+            for (int i = 0; i < T; ++i) {
+                IRunner runner = new Runner(br, bw);
+                runner.run();
+                runner.flush();
+            }
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Solution {
+
+    final int[][] times;
+
+    Solution(int[][] times) {
+        this.times = times;
+    }
+
+    public int solution() {
+        Arrays.sort(times, (a, b) -> {
+            if (a[1] == b[1]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+
+        int t = 0, count = 0;
+        for (int[] time : times) {
+            if (t <= time[0]) {
+                t = time[1];
+                ++count;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+class Runner implements IRunner {
+    final IReader reader;
+    final BufferedWriter bw;
+    final StringBuilder sb = new StringBuilder();
+
+    final int[][] times;
+
+    Runner(BufferedReader br, BufferedWriter bw) {
+        this.reader = new Reader(br);
+        this.bw = bw;
+        try {
+            var N = reader.readInts()[0];
+            this.times = new int[N][];
+            for (int i = 0; i < N; ++i) {
+                this.times[i] = reader.readInts();
+            }
+
+            sb.ensureCapacity(20);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void flush() {
+        try {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() throws IOException {
+        var res = new Solution(times).solution();
+        sb.append(res).append('\n');
+    }
+}
+
+class Reader implements IReader {
+    private BufferedReader br;
+
+    public Reader(BufferedReader br) {
+        this.br = br;
+    }
+
+    @Override
+    public void skipLine() throws IOException {
+        br.readLine();
+    }
+
+    @Override
+    public Stream<String> lines() throws IOException {
+        return br.lines();
+    }
+
+    @Override
+    public String line() throws IOException {
+        return br.readLine();
+    }
+
+    @Override
+    public int[] readInts() throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        int[] ints = new int[tokens.length];
+        for (int i = 0; i < tokens.length; ++i) {
+            ints[i] = Integer.parseInt(tokens[i]);
+        }
+        return ints;
+    }
+
+    @Override
+    public List<Integer> readIntegers() throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        var res = new ArrayList<Integer>(tokens.length);
+        for (String token : tokens) {
+            res.add(Integer.valueOf(token));
+        }
+        return res;
+    }
+}
+
+interface IRunner {
+    void run() throws IOException;
+
+    void flush();
+}
+
+interface IReader {
+    void skipLine() throws IOException;
+
+    Stream<String> lines() throws IOException;
+
+    String line() throws IOException;
+
+    List<Integer> readIntegers() throws IOException;
+
+    int[] readInts() throws IOException;
+}
