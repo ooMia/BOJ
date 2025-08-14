571acf619770bf455106ff907eceeb6e0ce497cd
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-14 14:38:17 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 380 ms, Memory: 215116 KB, Score: 201 point -BaekjoonHub

---
+++ Java/백준/Silver/15624. 피보나치 수 7/피보나치 수 7.java
@@ -0,0 +1,147 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int T = 1;
+            for (int i = 0; i < T; ++i) {
+                IRunner runner = new Runner(br, bw);
+                runner.run();
+                runner.flush();
+            }
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Solution {
+
+    final int n;
+    final BigInteger mod = BigInteger.valueOf(1_000_000_007);
+    final BigInteger[] fibo;
+
+    Solution(int n) {
+        this.n = n;
+        this.fibo = new BigInteger[n + 2];
+        fibo[0] = BigInteger.ZERO;
+        fibo[1] = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i - 1].add(fibo[i - 2]).mod(mod);
+        }
+    }
+
+    public BigInteger solution() {
+        return fibo[n];
+    }
+}
+
+class Runner implements IRunner {
+    final IReader reader;
+    final BufferedWriter bw;
+    final StringBuilder sb = new StringBuilder();
+
+    final int n;
+
+    Runner(BufferedReader br, BufferedWriter bw) {
+        this.reader = new Reader(br);
+        this.bw = bw;
+        try {
+            this.n = reader.readInts()[0];
+
+            sb.ensureCapacity(20);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void flush() {
+        try {
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() throws IOException {
+        var sol = new Solution(n);
+        var res = sol.solution();
+        sb.append(res).append('\n');
+    }
+}
+
+class Reader implements IReader {
+    private BufferedReader br;
+
+    public Reader(BufferedReader br) {
+        this.br = br;
+    }
+
+    @Override
+    public void skipLine() throws IOException {
+        br.readLine();
+    }
+
+    @Override
+    public Stream<String> lines() throws IOException {
+        return br.lines();
+    }
+
+    @Override
+    public String line() throws IOException {
+        return br.readLine();
+    }
+
+    @Override
+    public int[] readInts() throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        int[] ints = new int[tokens.length];
+        for (int i = 0; i < tokens.length; ++i) {
+            ints[i] = Integer.parseInt(tokens[i]);
+        }
+        return ints;
+    }
+
+    @Override
+    public List<Integer> readIntegers() throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        var res = new ArrayList<Integer>(tokens.length);
+        for (String token : tokens) {
+            res.add(Integer.valueOf(token));
+        }
+        return res;
+    }
+}
+
+interface IRunner {
+    void run() throws IOException;
+
+    void flush();
+}
+
+interface IReader {
+    void skipLine() throws IOException;
+
+    Stream<String> lines() throws IOException;
+
+    String line() throws IOException;
+
+    List<Integer> readIntegers() throws IOException;
+
+    int[] readInts() throws IOException;
+}
