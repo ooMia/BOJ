18ad7868a0b4f59f751bae8feffd1ce4cc12bdf5
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-15 22:38:27 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 84.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ Java/프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,24 +1,40 @@
-import java.util.TreeMap;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[][] score) {
-        var map = new TreeMap<Integer, Integer>();
-        for (var s: score) {
-            var k = s[0] + s[1];
-            map.put(k, map.getOrDefault(k,0) + 1);
-        }
+        var N = score.length;
+        Integer[] ranks = new Integer[N];
+        for (int i=0; i<N; ++i)
+            ranks[i] = score[i][0] + score[i][1];
         
-        int rank = 1;
-        for (var e: map.descendingMap().entrySet()) {
-            var k = e.getKey();
-            var v = e.getValue();
-            map.put(k, rank);
-            rank += v;
-        }
+        Arrays.sort(ranks);
         
-        int[] answer = new int[score.length];
-        for (int i=0; i<score.length; ++i)
-            answer[i] = map.get(score[i][0] + score[i][1]);
+        int[] answer = new int[N];
+        for (int i=0; i<N; ++i) {
+            answer[i] = N - binarySearch(ranks, score[i][0] + score[i][1]);
+        }
         return answer;
+    }
+    
+    int binarySearch(Integer[] a, int key) {
+        // 기존 Arrays.sort는 동일 값에 대해 최소 인덱스를 반환하므로 수정
+        int low = 0;
+        int high = a.length - 1;
+        int result = -1; // 최대 인덱스 저장 변수
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            long midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                result = mid;
+                low = mid + 1;
+            }
+        }
+        return result != -1 ? result : -(low + 1);
     }
 }
