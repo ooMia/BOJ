a9d25909eb5885f618afbb2613773693a8a9c256
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-17 01:40:45 +0900

    [level 0] Title: 이진수 더하기, Time: 0.05 ms, Memory: 95 MB -BaekjoonHub

--- Java/프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ Java/프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -2,10 +2,8 @@ import java.math.BigInteger;
 
 class Solution {
     public String solution(String bin1, String bin2) {
-        BigInteger num1 = new BigInteger(bin1, 2);
-        BigInteger num2 = new BigInteger(bin2, 2);
-        BigInteger sum = num1.add(num2);
-
-        return sum.toString(2);
+        int n1 = Integer.parseInt(bin1, 2);
+        int n2 = Integer.parseInt(bin2, 2);
+        return Integer.toString(n1+n2, 2);
     }
 }
