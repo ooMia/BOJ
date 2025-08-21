22f38545972ccaf0e4a096545c89c113fbc38a88
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-08-22 04:56:02 +0900

    [level 0] Title: 코드 처리하기, Time: 8.03 ms, Memory: 73.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ Java/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,10 +1,9 @@
 class Solution {
-    public String solution(String _code) {
+    public String solution(String code) {
         int mode = 0;
         var sb = new StringBuilder();
-        var code = _code.toCharArray();
-        for (int idx=0; idx<code.length; ++idx) {
-            var c = code[idx];
+        for (int idx=0; idx<code.length(); ++idx) {
+            var c = code.charAt(idx);
             if (c == '1') mode ^= 1;
             else if (idx % 2 == mode) sb.append(c); 
         }
