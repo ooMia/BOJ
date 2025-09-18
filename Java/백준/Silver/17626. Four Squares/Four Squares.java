25099fdac780d934142a78557e61f84c773a9b53
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-18 21:52:10 +0900

    [Silver III] Title: Four Squares, Time: 120 ms, Memory: 14512 KB -BaekjoonHub

--- Java/백준/Silver/17626. Four Squares/Four Squares.java
+++ Java/백준/Silver/17626. Four Squares/Four Squares.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -29,45 +27,22 @@ public class Main {
 
 class Solution {
     final int n;
-    final int[] squares;
+    final int[] dp;
 
     Solution(int n) {
         this.n = n;
-
-        int l = (int) Math.sqrt(n);
-        squares = new int[l];
-        for (int k = 1; k <= l; ++k)
-            squares[k - 1] = k * k;
+        this.dp = new int[n + 1];
     }
 
     int solution() {
-        PriorityQueue<Task> pq = new PriorityQueue<>(Comparator.comparingInt(t -> t.count));
-        pq.offer(new Task(n, 0));
-
-        while (!pq.isEmpty()) {
-            Task curr = pq.poll();
-            if (curr.n == 0)
-                return curr.count;
-
-            int iStart = Arrays.binarySearch(squares, curr.n / 3);
-            iStart = iStart < 0 ? -(iStart + 1) : iStart;
-            for (int i = iStart; i < squares.length; ++i) {
-                int square = squares[i];
-                if (square > curr.n)
-                    break;
-                pq.offer(new Task(curr.n - square, curr.count + 1));
+        for (int i = 1; i <= n; ++i) {
+            int ans = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+                ans = Math.min(ans, dp[i - j * j] + 1);
             }
+            dp[i] = ans;
         }
-        return -1;
-    }
-
-    class Task {
-        int n, count;
-
-        Task(int n, int count) {
-            this.n = n;
-            this.count = count;
-        }
+        return dp[n];
     }
 }
 
