ebf24cbe519fc00046991ce7e91f17a42ace96e8
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-26 19:02:09 +0900

    [Bronze II] Title: 이진수 합, Time: 220 ms, Memory: 21892 KB -BaekjoonHub

--- Java/백준/Bronze/8741. 이진수 합/이진수 합.java
+++ Java/백준/Bronze/8741. 이진수 합/이진수 합.java
@@ -27,6 +27,9 @@ public class Main {
 
 class Solution {
     String solution(int k) {
+        // S = (2^k - 1) * (2^k) / 2
+        //   = (2^k - 1) * 2^(k-1)
+        //   = (2^k - 1) << (k-1)
         var sb = new StringBuilder();
         for (int i = 0; i < k; ++i) sb.append('1');
         for (int i = 0; i < k - 1; ++i) sb.append('0');
