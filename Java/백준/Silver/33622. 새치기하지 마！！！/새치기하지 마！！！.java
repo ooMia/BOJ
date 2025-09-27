8f0230accb19ea55d7b6edc1f38d35f99cb054e0
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-09-27 18:53:45 +0900

    [Silver V] Title: 새치기하지 마!!!, Time: 108 ms, Memory: 14588 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Silver/33622. 새치기하지 마！！！/새치기하지 마！！！.java
+++ Java/백준/Silver/33622. 새치기하지 마！！！/새치기하지 마！！！.java
@@ -26,54 +26,8 @@ public class Main {
 }
 
 class Solution {
-    private final double[] probs;
-    private final int[][] choices;
-    private final int N;
-
-    Solution(int N) {
-        this.N = N;
-        int size = Math.max(4, N + 1);
-        probs = new double[size];
-        choices = new int[size][];
-
-        probs[0] = 1.0;
-        probs[1] = probability(1, 1);
-        probs[2] = probability(2, 2);
-        probs[3] = probability(3, 3);
-
-        choices[0] = new int[]{};
-        choices[1] = new int[]{1};
-        choices[2] = new int[]{2};
-        choices[3] = new int[]{3};
-
-        for (int target = 1; target <= N; ++target) {
-            for (int k = 1; k <= target; ++k) {
-                double trial = probs[target - k] * probability(target, k);
-                if (trial > probs[target]) {
-                    probs[target] = trial;
-                    int newSize = choices[target - k].length + 1;
-                    choices[target] = new int[newSize];
-                    System.arraycopy(choices[target - k], 0, choices[target], 0, newSize - 1);
-                    choices[target][newSize - 1] = k;
-                }
-            }
-        }
-    }
-
-    private static double probability(int n, int k) {
-        return 1.0 - (double) k / (n + 1);
-    }
-
-    String solution() {
-        int X = choices[N].length;
-        var history = choices[N];
-
-        var sb = new StringBuilder();
-        sb.append(X).append('\n');
-        for (int hist : history) {
-            sb.append(hist).append(' ');
-        }
-        return sb.toString().trim();
+    String solution(int N) {
+        return String.format("1\n%d", N);
     }
 }
 
@@ -103,8 +57,8 @@ class Runner {
     }
 
     public void run() throws IOException {
-        var sol = new Solution(N);
-        var res = sol.solution();
+        var sol = new Solution();
+        var res = sol.solution(N);
         _write(res);
     }
 
@@ -122,32 +76,5 @@ class Reader {
 
     public Reader(BufferedReader br) {
         this.br = br;
-    }
-
-    public int[] readInts() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int cnt = st.countTokens();
-        int[] ints = new int[cnt];
-        for (int i = 0; i < cnt; ++i) {
-            ints[i] = Integer.parseInt(st.nextToken());
-        }
-        return ints;
-    }
-
-    public long[] readLongs() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int cnt = st.countTokens();
-        long[] longs = new long[cnt];
-        for (int i = 0; i < cnt; ++i) {
-            longs[i] = Long.parseLong(st.nextToken());
-        }
-        return longs;
-    }
-
-    public int[][] readIntArrays() throws IOException {
-        return br.lines().map(s -> Arrays.stream(s.split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray())
-                .toArray(int[][]::new);
     }
 }
