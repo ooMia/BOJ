9ec2c365286d1984dcfc50288c479e680091a913
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-06 11:03:15 +0900

    [Silver IV] Title: 수강변경, Time: 736 ms, Memory: 188188 KB -BaekjoonHub

--- Java/백준/Silver/23305. 수강변경/수강변경.java
+++ Java/백준/Silver/23305. 수강변경/수강변경.java
@@ -27,38 +27,27 @@ public class Main {
 
 class Solution {
 
+    final static int maxClassNumber = 1_000_000;
+    final static int[] have = new int[maxClassNumber + 1];  // have[i] = 수업 i를 가진 학생 수
+    final static int[] want = new int[maxClassNumber + 1];  // want[i] = 수업 i를 원하는 학생 수
+
     // N: 학생 수
     // cur: i번 학생의 현재 수업 배열
     // seek: i번 학생의 원하는 수업 배열
     public int solution(int N, int[] cur, int[] seek) {
-        int alreadySatisfied = 0;
-        int maxClassNumber = -1;
-        for (int i = 0; i < N; i++) {
-            maxClassNumber = Math.max(maxClassNumber, Math.max(cur[i], seek[i]));
-            if (cur[i] == seek[i]) {
-                alreadySatisfied++;
-            }
-        }
-
         // 각 수업별로 현재 가진 학생 수와 원하는 학생 수 계산
-        int[] have = new int[maxClassNumber + 1];  // have[i] = 수업 i를 가진 학생 수
-        int[] want = new int[maxClassNumber + 1];  // want[i] = 수업 i를 원하는 학생 수
-
-        for (int i = 0; i < N; i++) {
-            if (cur[i] != seek[i]) {  // 교환이 필요한 학생만
-                have[cur[i]]++;
-                want[seek[i]]++;
-            }
+        for (int i = 0; i < N; ++i) {
+            ++have[cur[i]];
+            ++want[seek[i]];
         }
 
-        // 각 수업별로 교환 가능한 학생 수 계산
+        // 각 수업별로 교환 가능한 학생 수
         int canExchange = 0;
-        for (int i = 1; i <= maxClassNumber; i++) {
-            // 수업 i에 대해 교환 가능한 학생 수는 min(가진 학생 수, 원하는 학생 수)
+        for (int i = 1; i <= maxClassNumber; ++i) {
             canExchange += Math.min(have[i], want[i]);
         }
 
-        return N - alreadySatisfied - canExchange;
+        return N - canExchange;
     }
 }
 
